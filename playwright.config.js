4660bd69273f8138552c667c4ff0c6b4586284b9
Furkiozknn <121863222+Furkiozknn@users.noreply.github.com>
2026-09-09 07:31:40 +0300

    test: yerelde playwright işçi sayısını 1'e sabitle (RAM yetersizliğinde OOM)

--- playwright.config.js
+++ playwright.config.js
@@ -6,6 +6,9 @@ module.exports = defineConfig({
   testDir: './test',
   timeout: 30000,
   fullyParallel: true,
+  // Yerelde 1 işçi: makinede 8 GB RAM var, paralel çalıştırma worker'ları
+  // "JavaScript heap out of memory" ile düşürüyor. CI'da sınır yok.
+  workers: process.env.CI ? undefined : 1,
   reporter: [['list']],
   webServer: {
     command: 'python3 -m http.server 4174',
